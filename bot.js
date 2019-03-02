89aec3770780bc9b4bc7fe58723135ef133bde67
BodyIsKing <47182494+BodyIsKing@users.noreply.github.com>
2019-03-02 21:41:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("PoliteShop || Brodcast",{type: 'WATCHING'})
+   client.user.setActivity("PoliteServer || Brodcast",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('+[-----------------------------------------------------------------]+')
@@ -165,7 +165,7 @@ m.sendMessage(args)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("PoliteBrodcast || ByRD.",{type: 'WATCHING'});
+          client.user.setActivity("PoliteBrodcast  ",{type: 'WATCHING'});
   
   });
 
